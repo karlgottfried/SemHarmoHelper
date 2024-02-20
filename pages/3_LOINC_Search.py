f2e8864a7d9c3f871a8e7a5b8b1b3b2ad997ee15
kttrld <k.gottfried@uke.de>
2024-02-20 22:14:48 +0100

    translate Loinc Search to english

--- pages/3_LOINC_Search.py
+++ pages/3_LOINC_Search.py
@@ -3,59 +3,61 @@ import requests
 from requests.auth import HTTPBasicAuth
 import pandas as pd
 
-st.title('LOINC FHIR Fragebögen')
+st.title('LOINC FHIR Questionnaires')
 
-# Initialisieren Sie die Session State Variablen für Benutzername und Passwort, wenn sie noch nicht gesetzt sind
+# Initialize session state variables for username and password if they are not set
 if 'username' not in st.session_state:
     st.session_state['username'] = ''
 if 'password' not in st.session_state:
     st.session_state['password'] = ''
 
 
-# Funktion, um die Anmeldeinformationen zu setzen
+# Function to set credentials
 def set_credentials():
-    st.session_state['username'] = st.sidebar.text_input("Benutzername")
-    st.session_state['password'] = st.sidebar.text_input("Passwort", type="password")
+    st.session_state['username'] = st.sidebar.text_input("Username")
+    st.session_state['password'] = st.sidebar.text_input("Password", type="password")
 
 
 def load_questionnaires(url):
+    # Check if username and password are entered
     if st.session_state['username'] and st.session_state['password']:
         response = requests.get(url, auth=HTTPBasicAuth(st.session_state['username'], st.session_state['password']))
         if response.status_code == 200:
-            return response.json()  # Annahme, dass die Antwort JSON-Daten enthält
+            return response.json()  # Assuming the response contains JSON data
         else:
-            st.error("Fehler beim Laden der Fragebögen.")
+            st.error("Error loading questionnaires.")
             return None
     else:
-        st.warning("Bitte geben Sie Benutzername und Passwort ein.")
+        st.warning("Please enter username and password.")
         return None
 
 
-# Funktion, um Fragebögen anzuzeigen
+# Function to display questionnaires
 def display_questionnaires(data):
     if data:
         st.write(data["item"])
     else:
-        st.write("Keine Daten gefunden.")
+        st.write("No data found.")
 
 
-# Funktion zum Extrahieren der Daten
-def extract_data(title, copyr, json_data):
+# Function to extract data
+def extract_data(title, copyright, json_data):
     rows = []
     for item in json_data:
-        for code in item.get("code", []):  # Für den Fall, dass "code" nicht vorhanden ist
-            # Extrahieren der Antwortoptionen als String mit Zeilenumbrüchen
+        # In case 'code' is not present
+        for code in item.get("code", []):
+            # Extract response options as a string with line breaks
             answer_options = "\n".join([opt["valueCoding"]["display"] + " | " for opt in item.get("answerOption", [])])
-            # Hinzufügen der extrahierten Daten zu einer Liste von Zeilen
+            # Add extracted data to a list of rows
             rows.append({
                 "ID": item["linkId"],
                 "Questionnaire": title,
                 "Code": code["code"],
                 "Question (Display)": code["display"],
                 "Response (Display)": answer_options,
-                "copyright": copyr
+                "Copyright": copyright
             })
-    # Erstellen eines DataFrames aus der Liste von Zeilen
+    # Create a DataFrame from the list of rows
     return pd.DataFrame(rows)
 
 
@@ -67,34 +69,34 @@ codes = {
     "Generalized anxiety disorder 7 item": "69737-5"
 }
 
-# MultiSelect-Box für die Auswahl mehrerer Fragebögen
-selected_names = st.multiselect("LOINC-Codes", list(codes.keys()))
+# MultiSelect box to select multiple questionnaires
+selected_names = st.multiselect("LOINC Codes", list(codes.keys()))
 
-# URLs basierend auf den ausgewählten Codes generieren
+# Generate URLs based on selected codes
 urls = [f"https://fhir.loinc.org/Questionnaire/{codes[name]}" for name in selected_names]
 
-# Eine leere Liste für die Datenframes initialisieren
+# Initialize an empty list for the dataframes
 dfs = []
 
-# Button, um das Laden der Fragebögen zu starten
-if st.button("Fragebögen laden"):
+# Button to start loading the questionnaires
+if st.button("Load questionnaires"):
     for url in urls:
-        # Lade die Fragebogendaten für jede ausgewählte URL
+        # Load questionnaire data for each selected URL
         data = load_questionnaires(url)
-        # Extrahiere Daten und füge das DataFrame der Liste hinzu
+        # Extract data and add the DataFrame to the list
         df = extract_data(data["title"], data["copyright"], data["item"])
 
         with st.expander(data["title"]):
-            # Expander für jeden Fragebogen
-            st.write(df)  # Anzeigen der Fragebogentabelle im Expander
+            # Expander for each questionnaire
+            st.write(df)  # Display the questionnaire table in the expander
             st.info(data["copyright"])
 
         dfs.append(df)
 
-    # Kombiniere alle DataFrames in der Liste zu einem einzigen DataFrame
+    # Combine all DataFrames in the list into a single DataFrame
     if dfs:
-        st.write("Gesamte Ausgabetabelle aller ausgewählten Fragebögen:")
+        st.write("Combined table of all selected questionnaires:")
         combined_df = pd.concat(dfs, ignore_index=True)
         st.write(combined_df)
     else:
-        st.error("Keine Daten geladen. Bitte wähle mindestens einen Fragebogen aus.")
+        st.error("No data loaded. Please select at least one questionnaire.")
